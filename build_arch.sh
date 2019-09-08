21941805f25a4c323a748153841ad19122595cad
xRoBoTx <pearpieteam@gmail.com>
2019-09-08 23:02:06 +0700

    Added some files.

--- build_arch.sh
+++ build_arch.sh
@@ -1,24 +1,7 @@
-mkdir libs
-cd libs
-#Clone pistache library
-git clone https://github.com/oktal/pistache.git
-cd pistache
-git submodule update --init
-mkdir build
-cd build
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
-make
-sudo make install
-
-cd ../..
-
-#Clone libpqcxx library
-git clone https://github.com/jtv/libpqxx.git
-cd libpqcxx
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+#Init submodules
+git submodule update --init --recursive
 
 #Build project
-cd ../..
 mkdir build
 cd build
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
