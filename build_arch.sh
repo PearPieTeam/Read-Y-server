c2c8a6441aaa45211817ce2546dfbc653055176f
xRoBoTx <pearpieteam@gmail.com>
2019-09-08 14:27:04 +0700

    Added some files.

--- build_arch.sh
+++ build_arch.sh
@@ -1,2 +1,13 @@
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release 
-make 
+mkdir libs
+cd libs
+git clone https://github.com/oktal/pistache.git
+git submodule update --init
+cd pistache
+mkdir build
+cd build
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
+make
+sudo make install
+cd ../..
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
+make
